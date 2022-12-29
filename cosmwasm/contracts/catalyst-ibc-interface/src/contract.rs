88598576030eade9652b2e2d8b70db2e47056985
Jorge Sanmiguel <sanmiguelgimeno@hotmail.com>
2022-12-29 16:00:59 +0100

    feat: CW Add instantiate to IBC interface contract

--- cosmwasm/contracts/catalyst-ibc-interface/src/contract.rs
+++ cosmwasm/contracts/catalyst-ibc-interface/src/contract.rs
@@ -1,26 +1,39 @@
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
 use cosmwasm_std::{Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
-// use cw2::set_contract_version;
+use cw2::set_contract_version;
 
 use crate::error::ContractError;
 use crate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use crate::state::{CatalystIBCInterfaceState, CATALYST_IBC_INTERFACE_STATE};
 
-/*
 // version info for migration info
 const CONTRACT_NAME: &str = "crates.io:catalyst-ibc-interface";
 const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
-*/
+
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn instantiate(
-    _deps: DepsMut,
+    deps: DepsMut,
     _env: Env,
     _info: MessageInfo,
-    _msg: InstantiateMsg,
+    msg: InstantiateMsg,
 ) -> Result<Response, ContractError> {
-    unimplemented!()
+
+    set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
+
+    let catalyst_ibc_interface_state = CatalystIBCInterfaceState {
+        ibc_endpoint: deps.api.addr_validate(&msg.ibc_endpoint)?   // Validate ibc_endpoint
+    };
+
+    CATALYST_IBC_INTERFACE_STATE.save(deps.storage, &catalyst_ibc_interface_state)?;
+
+    Ok(
+        Response::new()
+            .add_attribute("ibc_endpoint", catalyst_ibc_interface_state.ibc_endpoint)
+    )
 }
+
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn execute(
@@ -38,4 +51,32 @@ pub fn query(_deps: Deps, _env: Env, _msg: QueryMsg) -> StdResult<Binary> {
 }
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use super::*;
+    use cosmwasm_std::{testing::{mock_dependencies, mock_env, mock_info}, Attribute};
+
+    pub const IBC_ENDPOINT_TEST_ADDR: &str = "inst_addr";
+
+    #[test]
+    fn test_instantiate() {
+
+        let mut deps = mock_dependencies();
+        let env = mock_env();
+        let info = mock_info(IBC_ENDPOINT_TEST_ADDR, &vec![]);
+      
+        let msg = InstantiateMsg {
+            ibc_endpoint: IBC_ENDPOINT_TEST_ADDR.to_string()
+        };
+      
+        // Instantiate contract
+        let response = instantiate(deps.as_mut(), env, info, msg).unwrap();
+
+        // Verify response attributes
+        assert_eq!(
+            response.attributes[0],
+            Attribute { key: "ibc_endpoint".to_string(), value: IBC_ENDPOINT_TEST_ADDR.to_string() }
+        );
+
+        // TODO Verify state
+    }
+}
--- cosmwasm/contracts/catalyst-ibc-interface/src/state.rs
+++ cosmwasm/contracts/catalyst-ibc-interface/src/state.rs
@@ -1,1 +1,10 @@
+use cosmwasm_schema::cw_serde;
+use cosmwasm_std::Addr;
+use cw_storage_plus::Item;
 
+pub const CATALYST_IBC_INTERFACE_STATE: Item<CatalystIBCInterfaceState> = Item::new("catalyst-ibc-interface-state");
+
+#[cw_serde]
+pub struct CatalystIBCInterfaceState {
+    pub ibc_endpoint: Addr
+}
