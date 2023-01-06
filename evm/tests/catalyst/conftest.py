f17511c74dba40d3a977b29f188d9ca69a15471c
Alexander <alexander@lindgren.xyz>
2023-01-06 09:38:10 +0100

    tests: amplified pools are also param now

--- evm/tests/catalyst/conftest.py
+++ evm/tests/catalyst/conftest.py
@@ -7,7 +7,8 @@ from brownie import (
     ZERO_ADDRESS,
 )
 from brownie import convert, accounts
-from volatile.volatile_pools import volatile_params
+from pool_data import pool_information
+from itertools import combinations
 
 CHANNEL_ID = 0
 
@@ -28,22 +29,76 @@ def isolation(fn_isolation):
 
 def pytest_addoption(parser):
     parser.addoption("--poolname", help="Only runs tests for poolname")
+    parser.addoption("--amppoolname", help="Only runs tests for amppoolname")
 
 
 def pytest_generate_tests(metafunc):
+    volatile_pools = {}
+    amplified_pools = {}
+    for key, value in pool_information.items():
+        if value.get("amp") == 2**64:
+            volatile_pools[key] = value
+        else:
+            amplified_pools[key] = value
+    
+    
     if "pool_data" in  metafunc.fixturenames:
         poolname = metafunc.config.getoption('poolname')
         if poolname is not None:
+            assert 1 < len(volatile_pools[poolname].get("tokens"))
+            assert 2**64 == len(volatile_pools[poolname].get("amp"))
             metafunc.parametrize("pool_data", [poolname], indirect=True, scope="session")
         else:
-            metafunc.parametrize("pool_data", list(volatile_params.keys()), indirect=True, scope="session")
+            localSwapPools = []
+            for key, value in volatile_pools.items():
+                if 1 < len(value.get("tokens")):
+                    localSwapPools.append(key)
+            metafunc.parametrize("pool_data", localSwapPools, indirect=True, scope="session")
+    
+    
+    if "pool_data_cross" in  metafunc.fixturenames:
+        multiSwapPools = []
+        ids = []
+        print(list(combinations(volatile_pools.items(), 2)))
+        for pool1, pool2 in combinations(volatile_pools.items(), 2):
+            key1, value1, key2, value2 = pool1 + pool2
+            allTokens = value1.get("tokens") + value2.get("tokens")
+            if len(set(allTokens)) == len(allTokens):
+                multiSwapPools.append([key1, key2])
+                ids.append(key1 + "+" + key2)
+        metafunc.parametrize("pool_data_cross", multiSwapPools, ids=ids, indirect=True, scope="session")
+    
+    
+    if "amp_pool_data" in  metafunc.fixturenames:
+        amppoolname = metafunc.config.getoption('amppoolname')
+        if amppoolname is not None:
+            assert 1 < len(amplified_pools[amppoolname].get("tokens"))
+            metafunc.parametrize("pool_data", [amppoolname], indirect=True, scope="session")
+        else:
+            localSwapPools = []
+            for key, value in amplified_pools.items():
+                if 1 < len(value.get("tokens")):
+                    localSwapPools.append(key)
+            metafunc.parametrize("amp_pool_data", localSwapPools, indirect=True, scope="session")
+    
+    
+    if "amp_pool_data_cross" in  metafunc.fixturenames:
+        multiSwapPools = []
+        ids = []
+        print(list(combinations(amplified_pools.items(), 2)))
+        for pool1, pool2 in combinations(amplified_pools.items(), 2):
+            key1, value1, key2, value2 = pool1 + pool2
+            allTokens = value1.get("tokens") + value2.get("tokens")
+            if len(set(allTokens)) == len(allTokens):
+                multiSwapPools.append([key1, key2])
+                ids.append(key1 + "+" + key2)
+        metafunc.parametrize("amp_pool_data_cross", multiSwapPools, ids=ids, indirect=True, scope="session")
             
-
 
 # Pool data given to create pools for tests.
 @pytest.fixture(scope="session")
 def pool_data(request):
-    base_data = volatile_params[request.param]
+    base_data = pool_information[request.param]
     return {
         **base_data, 
         "deployer": accounts[base_data.get("deployer")]
@@ -51,58 +106,35 @@ def pool_data(request):
 
 
 @pytest.fixture(scope="session")
-def pool_data_cross():
-    return [dict(
-        amp=2**64,
-        tokens = [0, 1],
-        depositAmounts=[10 * 10**18, 1000 * 10**18],
-        weights=[1,1,1],
-        poolName="POOLNAME",
-        poolSymbol="PS",
-        deployer=accounts[1]
-    ), dict(
-        amp=2**64,
-        tokens = [2],
-        depositAmounts=[1000 * 10**6],
-        weights=[1,1,1],
-        poolName="POOLNAME2",
-        poolSymbol="PS2",
-        deployer=accounts[1]
-    )]
+def pool_data_cross(request):
+    pool1, pool2 = request.param
+    base_data1, base_data2 = pool_information[pool1], pool_information[pool2]
+    return [{
+        **base_data1, 
+        "deployer": accounts[base_data1.get("deployer")]
+    }, {
+        **base_data2, 
+        "deployer": accounts[base_data2.get("deployer")]
+    }]
 
 
 @pytest.fixture(scope="session")
-def amp_pool_data():
-    amp = 2**62
-    return dict(
-        amp=amp,
-        tokens = [0, 1, 2],
-        depositAmounts=[10 * 10**18, 1000 * 10**18, 1000 * 10**6],
-        weights=[int(int(1000 * 10**18)/int(10 * 10**18)), 1, int(int(1000 * 10**18)/int(1000 * 10**6))],
-        poolName="POOLNAME",
-        poolSymbol="PS",
-        deployer=accounts[1],
-        selfConnection=True  # TODO: Figure out how to parametrize setting this to 0. (should skip tests which requires local cross-chain swaps)
-    )
+def amp_pool_data(request):
+    base_data = pool_information[request.param]
+    return {
+        **base_data, 
+        "deployer": accounts[base_data.get("deployer")]
+    }
 
 
 @pytest.fixture(scope="session")
-def amp_pool_data_cross():
-    amp = 2**62
-    return [dict(
-        amp=amp,
-        tokens = [0, 1],
-        depositAmounts=[10 * 10**18, 1000 * 10**18],
-        weights=[int(int(1000 * 10**18)/int(10 * 10**18)), 1],
-        poolName="POOLNAME",
-        poolSymbol="PS",
-        deployer=accounts[1]
-    ), dict(
-        amp=amp,
-        tokens = [2],
-        depositAmounts=[1000 * 10**6],
-        weights=[int(int(1000 * 10**18)/int(1000 * 10**6))],
-        poolName="POOLNAME2",
-        poolSymbol="PS2",
-        deployer=accounts[1]
-    )]
+def amp_pool_data_cross(request):
+    pool1, pool2 = request.param
+    base_data1, base_data2 = pool_information[pool1], pool_information[pool2]
+    return [{
+        **base_data1, 
+        "deployer": accounts[base_data1.get("deployer")]
+    }, {
+        **base_data2, 
+        "deployer": accounts[base_data2.get("deployer")]
+    }]
