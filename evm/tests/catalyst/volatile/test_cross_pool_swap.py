f2ffb3c87a78d8cddae806d987c765d23c7b25a9
Jorge Sanmiguel <sanmiguelgimeno@hotmail.com>
2023-01-10 12:02:11 +0100

    test: Add approx cross chain swap test

--- evm/tests/catalyst/volatile/test_cross_pool_swap.py
+++ evm/tests/catalyst/volatile/test_cross_pool_swap.py
@@ -44,4 +44,41 @@ def test_cross_pool_swap(channelId, swappool1, swappool2, token1, token3, berg, 
     else:
         assert purchased_tokens <= int(y*1.000001), "Swap returns more than theoretical"
         assert (y * 9 /10) <= purchased_tokens, "Swap returns less than 9/10 theoretical"
+
+
+
+@pytest.mark.no_call_coverage
+@given(swap_amount=strategy("uint256", max_value=10**17))
+def test_cross_pool_swap_approx(channelId, swappool1, swappool2, token1, token3, berg, deployer, compute_expected_swap, ibcemulator, swap_amount):
+    token1.transfer(berg, swap_amount, {'from': deployer})
+    token1.approve(swappool1, swap_amount, {'from': berg})
+    
+    y = compute_expected_swap(swap_amount, token1, token3, swappool1, swappool2)
+    
+    tx = swappool1.swapToUnits(
+        channelId,
+        convert.to_bytes(swappool2.address.replace("0x", "")),
+        convert.to_bytes(berg.address.replace("0x", "")),
+        token1,
+        0,
+        swap_amount,
+        0,
+        3,  # Equal to True, True
+        berg,
+        {"from": berg},
+    )
+    assert token1.balanceOf(berg) == 0
+    
+    # Given that we are leading with small swap amounts, the security limit should never be reached
+    txe = ibcemulator.execute(tx.events["IncomingMetadata"]["metadata"][0], tx.events["IncomingPacket"]["packet"], {"from": berg})
+    
+    purchased_tokens = txe.events["SwapFromUnits"]["output"]
+    
+    assert purchased_tokens == token3.balanceOf(berg)
+    
+    if swap_amount/(token1.balanceOf(swappool1)-swap_amount) < 1e-06:
+        assert purchased_tokens <= int(y*1.000001), "Swap returns more than theoretical"
+    else:
+        assert purchased_tokens <= int(y*1.000001), "Swap returns more than theoretical"
+        assert (y * 9 /10) <= purchased_tokens, "Swap returns less than 9/10 theoretical"
     
