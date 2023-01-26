e49ff9647f64597d62b9da91ed692bbdfe6d02c4
Jorge Sanmiguel <sanmiguelgimeno@hotmail.com>
2023-01-26 16:30:06 +0100

    test: Update security limit init test

--- evm/tests/catalyst/test_common/unit/test_security_limit_init.py
+++ evm/tests/catalyst/test_common/unit/test_security_limit_init.py
@@ -1,21 +1,13 @@
 from tests.catalyst.utils.common_utils import assert_abs_relative_error
 
 
-def test_security_limit_init(pool_1, get_pool_1_max_unit_inflow, pool_2, get_pool_2_max_unit_inflow):
+def test_security_limit_init(pool, get_pool_max_unit_inflow):
     """
         Make sure the security limit gets correctly initialized
     """
 
-    expected_source_max_capacity = get_pool_1_max_unit_inflow()
-    observed_source_max_capacity = pool_1._max_unit_inflow()
+    expected_source_max_capacity = get_pool_max_unit_inflow()
+    observed_source_max_capacity = pool._max_unit_inflow()
 
     assert_abs_relative_error(observed_source_max_capacity, expected_source_max_capacity, 1e-10)
-    assert observed_source_max_capacity == pool_1.getUnitCapacity()     # Since there have been no swaps, max capacity == current capacity
-
-
-    expected_target_max_capacity = get_pool_2_max_unit_inflow()
-    observed_target_max_capacity = pool_2._max_unit_inflow()
-
-    assert_abs_relative_error(observed_target_max_capacity, expected_target_max_capacity, 1e-10)
-    assert observed_target_max_capacity == pool_2.getUnitCapacity()     # Since there have been no swaps, max capacity == current capacity
-
+    assert observed_source_max_capacity == pool.getUnitCapacity()     # Since there have been no swaps, max capacity == current capacity
