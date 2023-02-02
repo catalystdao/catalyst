fdc6b03b93214f711582988d4d9a4a2c68444440
Alexander <alexander@lindgren.xyz>
2023-02-02 16:13:51 +0100

    test: Escrow functions can only be called from CCI (and you need proper escrow information)

--- evm/tests/catalyst/test_common/unit/test_incoming_swap_caller.py
+++ evm/tests/catalyst/test_common/unit/test_incoming_swap_caller.py
@@ -51,3 +51,110 @@ def test_inLiquidity_must_be_called_by_cci(
         web3.keccak(text="e"),
         {'from': cci}
     )
+    
+
+def test_release_escrow_must_be_called_cci(
+    pool,
+    berg,
+):
+    cci = pool._chaininterface()
+    
+    with reverts("dev: Only _chaininterface"):
+        pool.releaseEscrowACK(
+            web3.keccak(text="e"),
+            0,
+            0,
+            berg,
+            {'from': berg}
+        )
+    
+    # Since no swap has been executed, the escrow hash doesn't exist. However,
+    # we still want to check that we can get past the above requirement using
+    # a valid sender.
+    with reverts("dev: Invalid messageHash. Alt: Escrow doesn't exist."):
+        pool.releaseEscrowACK(
+            web3.keccak(text="e"),
+            0,
+            0,
+            berg,
+            {'from': cci}
+        )
+        
+def test_timeout_escrow_must_be_called_cci(
+    pool,
+    berg,
+):
+    cci = pool._chaininterface()
+    
+    with reverts("dev: Only _chaininterface"):
+        pool.releaseEscrowTIMEOUT(
+            web3.keccak(text="e"),
+            0,
+            0,
+            berg,
+            {'from': berg}
+        )
+    
+    # Since no swap has been executed, the escrow hash doesn't exist. However,
+    # we still want to check that we can get past the above requirement using
+    # a valid sender.
+    with reverts("dev: Invalid messageHash. Alt: Escrow doesn't exist."):
+        pool.releaseEscrowTIMEOUT(
+            web3.keccak(text="e"),
+            0,
+            0,
+            berg,
+            {'from': cci}
+        )
+
+
+def test_release_liquidity_escrow_must_be_called_cci(
+    pool,
+    berg,
+):
+    cci = pool._chaininterface()
+    
+    with reverts("dev: Only _chaininterface"):
+        pool.releaseLiquidityEscrowACK(
+            web3.keccak(text="e"),
+            0,
+            0,
+            {'from': berg}
+        )
+    
+    # Since no swap has been executed, the escrow hash doesn't exist. However,
+    # we still want to check that we can get past the above requirement using
+    # a valid sender.
+    with reverts("dev: Invalid messageHash. Alt: Escrow doesn't exist."):
+        pool.releaseLiquidityEscrowACK(
+            web3.keccak(text="e"),
+            0,
+            0,
+            {'from': cci}
+        )
+    
+
+def test_timeout_liquidity_escrow_must_be_called_cci(
+    pool,
+    berg,
+):
+    cci = pool._chaininterface()
+    
+    with reverts("dev: Only _chaininterface"):
+        pool.releaseLiquidityEscrowTIMEOUT(
+            web3.keccak(text="e"),
+            0,
+            0,
+            {'from': berg}
+        )
+    
+    # Since no swap has been executed, the escrow hash doesn't exist. However,
+    # we still want to check that we can get past the above requirement using
+    # a valid sender.
+    with reverts("dev: Invalid messageHash. Alt: Escrow doesn't exist."):
+        pool.releaseLiquidityEscrowTIMEOUT(
+            web3.keccak(text="e"),
+            0,
+            0,
+            {'from': cci}
+        )
