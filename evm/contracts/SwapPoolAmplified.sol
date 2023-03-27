88d59c5cb1f97404172bd5b23f845e617f678e73
jsanmigimeno <jorge@catalabs.org>
2023-03-27 15:57:02 +0200

    Closes #41 (#42)
    * feat: Add a maximum allowed amplification change
    
    * test: Overhaul amplification tests
    
    * fix: Modify max amp change check

--- evm/contracts/SwapPoolAmplified.sol
+++ evm/contracts/SwapPoolAmplified.sol
@@ -183,7 +183,14 @@ contract CatalystSwapPoolAmplified is CatalystSwapPoolCommon {
             require(targetTime >= block.timestamp + MIN_ADJUSTMENT_TIME); // dev: targetTime must be more than MIN_ADJUSTMENT_TIME in the future.
             require(targetTime <= block.timestamp + 365 days); // dev: Target time cannot be too far into the future.
         }
+
+        uint256 currentAmplification = FixedPointMathLib.WAD - uint256(_oneMinusAmp);
         require(targetAmplification < FixedPointMathLib.WAD);  // dev: amplification not set correctly.
+        // Limit the maximum allowed relative amplification change to a factor of 2. Note that this effectively 'locks'
+        // the amplification if it gets intialized to 0. Similarly, the amplification will never be allowed to be set to
+        // 0 if it is initialized to any other value (note how 'targetAmplification*2 >= currentAmplification' is used
+        // instead of 'targetAmplification >= currentAmplification/2').
+        require(targetAmplification <= currentAmplification*2 && targetAmplification*2 >= currentAmplification); // dev: targetAmplification must be maximum a factor of 2 larger/smaller than the current amplification to protect liquidity providers.
         // Because of the balance0 (_unitTracker) implementation, amplification adjustment has to be disabled for cross-chain pools.
         require(_chainInterface == address(0));  // dev: Amplification adjustment is disabled for cross-chain pools.
 
