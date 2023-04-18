5ea46d073b5d4be1f8e1c8cefbfba636ac26d92b
Jorge Sanmiguel <sanmiguelgimeno@hotmail.com>
2023-04-18 17:12:27 +0200

    test: Update 'pool_connections' test

--- cosmwasm/contracts/swap-pool-volatile/src/tests/test_pool_connections.rs
+++ cosmwasm/contracts/swap-pool-volatile/src/tests/test_pool_connections.rs
@@ -1,46 +1,48 @@
 mod test_volatile_pool_connections {
-    use cosmwasm_std::{testing::{mock_dependencies, mock_env, mock_info}, from_binary};
-    use swap_pool_common::ContractError;
+    use cosmwasm_std::Addr;
+    use cw_multi_test::{Executor, App};
+    use swap_pool_common::{ContractError, msg::PoolConnectionStateResponse};
 
-    use crate::{msg::VolatileExecuteMsg, tests::helpers::{mock_instantiate, SETUP_MASTER_ADDR, finish_pool_setup, FACTORY_OWNER_ADDR}, contract::{execute, query}};
+    use crate::{msg::VolatileExecuteMsg, tests::helpers::{mock_instantiate, SETUP_MASTER_ADDR, mock_finish_pool_setup, FACTORY_OWNER_ADDR}};
 
 
     #[test]
     fn test_set_connection() {
 
-        let mut deps = mock_dependencies();
-        mock_instantiate(deps.as_mut(), false);
+        let mut app = App::default();
+
+        // Instantiate vault
+        let vault = mock_instantiate(&mut app, false);
 
         let channel_id = "channel_0";
         let target_pool = b"target_pool".to_vec();
 
 
+
         // Tested action: set connection
-        let _response = execute(
-            deps.as_mut(),
-            mock_env(),
-            mock_info(SETUP_MASTER_ADDR, &[]),
-            VolatileExecuteMsg::SetConnection {
+        let _response = app.execute_contract::<VolatileExecuteMsg>(
+            Addr::unchecked(SETUP_MASTER_ADDR),
+            vault.clone(),
+            &VolatileExecuteMsg::SetConnection {
                 channel_id: channel_id.to_string(),
                 to_pool: target_pool.clone(),
                 state: true
-            }
+            },
+            &[]
         ).unwrap();
+
 
 
         // TODO verify response attributes (event)
 
         // Verify the connection is set
-        let queried_connection_state: bool = from_binary(
-            &query(
-                deps.as_ref(),
-                mock_env(),
-                crate::msg::QueryMsg::PoolConnectionState {
-                    channel_id: channel_id.to_string(),
-                    pool: target_pool
-                }
-            ).unwrap()
-        ).unwrap();
+        let queried_connection_state: bool = app.wrap().query_wasm_smart::<PoolConnectionStateResponse>(
+            vault.clone(),
+            &crate::msg::QueryMsg::PoolConnectionState {
+                channel_id: channel_id.to_string(),
+                pool: target_pool
+            }
+        ).unwrap().state;
 
         assert_eq!(
             queried_connection_state,
@@ -52,51 +54,50 @@ mod test_volatile_pool_connections {
     #[test]
     fn test_unset_connection() {
 
-        let mut deps = mock_dependencies();
-        mock_instantiate(deps.as_mut(), false);
+        let mut app = App::default();
+
+        // Instantiate vault
+        let vault = mock_instantiate(&mut app, false);
 
         let channel_id = "channel_0";
         let target_pool = b"target_pool".to_vec();
-        
+
         // Set the connection
-        let _response = execute(
-            deps.as_mut(),
-            mock_env(),
-            mock_info(SETUP_MASTER_ADDR, &[]),
-            VolatileExecuteMsg::SetConnection {
+        app.execute_contract::<VolatileExecuteMsg>(
+            Addr::unchecked(SETUP_MASTER_ADDR),
+            vault.clone(),
+            &VolatileExecuteMsg::SetConnection {
                 channel_id: channel_id.to_string(),
                 to_pool: target_pool.clone(),
                 state: true
-            }
+            },
+            &[]
         ).unwrap();
 
 
-        // TODO verify response attributes (event)
 
         // Tested action: unset the connection
-        let _response = execute(
-            deps.as_mut(),
-            mock_env(),
-            mock_info(SETUP_MASTER_ADDR, &[]),
-            VolatileExecuteMsg::SetConnection {
+        let _response = app.execute_contract::<VolatileExecuteMsg>(
+            Addr::unchecked(SETUP_MASTER_ADDR),
+            vault.clone(),
+            &VolatileExecuteMsg::SetConnection {
                 channel_id: channel_id.to_string(),
                 to_pool: target_pool.clone(),
                 state: false
-            }
+            },
+            &[]
         ).unwrap();
+
 
 
         // Verify the connection is not set
-        let queried_connection_state: bool = from_binary(
-            &query(
-                deps.as_ref(),
-                mock_env(),
-                crate::msg::QueryMsg::PoolConnectionState {
-                    channel_id: channel_id.to_string(),
-                    pool: target_pool
-                }
-            ).unwrap()
-        ).unwrap();
+        let queried_connection_state: bool = app.wrap().query_wasm_smart::<PoolConnectionStateResponse>(
+            vault.clone(),
+            &crate::msg::QueryMsg::PoolConnectionState {
+                channel_id: channel_id.to_string(),
+                pool: target_pool
+            }
+        ).unwrap().state;
 
         assert_eq!(
             queried_connection_state,
@@ -108,32 +109,34 @@ mod test_volatile_pool_connections {
     #[test]
     fn test_setup_master_after_setup_finish() {
 
-        let mut deps = mock_dependencies();
-        mock_instantiate(deps.as_mut(), false);
+        let mut app = App::default();
+
+        // Instantiate vault
+        let vault = mock_instantiate(&mut app, false);
 
         let channel_id = "channel_0";
         let target_pool = b"target_pool".to_vec();
 
         // Finish pool setup
-        finish_pool_setup(deps.as_mut());
+        mock_finish_pool_setup(&mut app, vault.clone());
 
 
         // Tested action: set connection
-        let response_result = execute(
-            deps.as_mut(),
-            mock_env(),
-            mock_info(SETUP_MASTER_ADDR, &[]),
-            VolatileExecuteMsg::SetConnection {
+        let response_result = app.execute_contract::<VolatileExecuteMsg>(
+            Addr::unchecked(SETUP_MASTER_ADDR),
+            vault.clone(),
+            &VolatileExecuteMsg::SetConnection {
                 channel_id: channel_id.to_string(),
                 to_pool: target_pool.clone(),
                 state: true
-            }
+            },
+            &[]
         );
 
 
         // Make sure SetConnection fails
         assert!(matches!(
-            response_result.err().unwrap(),
+            response_result.err().unwrap().downcast().unwrap(),
             ContractError::Unauthorized {}
         ));
     }
@@ -142,40 +145,39 @@ mod test_volatile_pool_connections {
     #[test]
     fn test_set_connection_factory_owner() {
 
-        let mut deps = mock_dependencies();
-        mock_instantiate(deps.as_mut(), false);
+        let mut app = App::default();
+
+        // Instantiate vault
+        let vault = mock_instantiate(&mut app, false);
 
         let channel_id = "channel_0";
         let target_pool = b"target_pool".to_vec();
 
         // Finish pool setup
-        finish_pool_setup(deps.as_mut());
+        mock_finish_pool_setup(&mut app, vault.clone());
 
 
         // Tested action: set connection invoked by factory owner
-        let _response = execute(
-            deps.as_mut(),
-            mock_env(),
-            mock_info(FACTORY_OWNER_ADDR, &[]),     // ! Invoked by the factory owner
-            VolatileExecuteMsg::SetConnection {
+        let _response = app.execute_contract::<VolatileExecuteMsg>(
+            Addr::unchecked(FACTORY_OWNER_ADDR),     // ! Invoked by the factory owner
+            vault.clone(),
+            &VolatileExecuteMsg::SetConnection {
                 channel_id: channel_id.to_string(),
                 to_pool: target_pool.clone(),
                 state: true
-            }
+            },
+            &[]
         ).unwrap();
 
 
         // Verify the connection is set
-        let queried_connection_state: bool = from_binary(
-            &query(
-                deps.as_ref(),
-                mock_env(),
-                crate::msg::QueryMsg::PoolConnectionState {
-                    channel_id: channel_id.to_string(),
-                    pool: target_pool
-                }
-            ).unwrap()
-        ).unwrap();
+        let queried_connection_state: bool = app.wrap().query_wasm_smart::<PoolConnectionStateResponse>(
+            vault.clone(),
+            &crate::msg::QueryMsg::PoolConnectionState {
+                channel_id: channel_id.to_string(),
+                pool: target_pool
+            }
+        ).unwrap().state;
 
         assert_eq!(
             queried_connection_state,
@@ -187,29 +189,31 @@ mod test_volatile_pool_connections {
     #[test]
     fn test_set_connection_invalid_caller() {
 
-        let mut deps = mock_dependencies();
-        mock_instantiate(deps.as_mut(), false);
+        let mut app = App::default();
+
+        // Instantiate vault
+        let vault = mock_instantiate(&mut app, false);
 
         let channel_id = "channel_0";
         let target_pool = b"target_pool".to_vec();
 
 
         // Tested action: set connection invoked by factory owner
-        let response_result = execute(
-            deps.as_mut(),
-            mock_env(),
-            mock_info("some_other_caller", &[]),     // ! Not setup master nor factory owner
-            VolatileExecuteMsg::SetConnection {
+        let response_result = app.execute_contract::<VolatileExecuteMsg>(
+            Addr::unchecked("some_other_caller"),     // ! Not setup master nor factory owner
+            vault.clone(),
+            &VolatileExecuteMsg::SetConnection {
                 channel_id: channel_id.to_string(),
                 to_pool: target_pool.clone(),
                 state: true
-            }
+            },
+            &[]
         );
 
 
         // Make sure SetConnection fails
         assert!(matches!(
-            response_result.err().unwrap(),
+            response_result.err().unwrap().downcast().unwrap(),
             ContractError::Unauthorized {}
         ));
     }
