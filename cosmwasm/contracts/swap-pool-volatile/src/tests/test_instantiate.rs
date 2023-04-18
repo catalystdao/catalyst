24e3ea24454977164e3e81a5dba8e19e96fa6304
Jorge Sanmiguel <sanmiguelgimeno@hotmail.com>
2023-04-18 12:58:40 +0200

    test: Migrate instantiate tests to cw_multi_test

--- cosmwasm/contracts/swap-pool-volatile/src/tests/test_instantiate.rs
+++ cosmwasm/contracts/swap-pool-volatile/src/tests/test_instantiate.rs
@@ -1,34 +1,42 @@
 
 mod test_volatile_instantiate {
-    use cosmwasm_std::{testing::{mock_dependencies, mock_env, mock_info}, from_binary, Uint128, Addr};
+    use cosmwasm_std::{Uint128, Addr};
     use cw20_base::state::TokenInfo;
+    use cw_multi_test::{App, Executor};
+    use swap_pool_common::msg::{SetupMasterResponse, ChainInterfaceResponse, OnlyLocalResponse, PoolFeeResponse, GovernanceFeeShareResponse};
 
-    use crate::{contract::{instantiate, query}, msg::QueryMsg, tests::helpers::{mock_instantiate_msg, DEPLOYER_ADDR}};
+    use crate::{msg::QueryMsg, tests::helpers::{mock_instantiate_msg, DEPLOYER_ADDR, volatile_vault_contract_storage}};
 
 
     #[test]
     fn test_instantiate() {
 
-        let mut deps = mock_dependencies();
+        let mut app = App::default();
+
         let chain_interface = Some("chain_interface".to_string());
         let instantiate_msg = mock_instantiate_msg(chain_interface);
 
 
         // Tested action: instantiate contract
-        let _response = instantiate(
-            deps.as_mut(),
-            mock_env(),
-            mock_info(DEPLOYER_ADDR, &vec![]),
-            instantiate_msg.clone()
+        let contract_code_storage = volatile_vault_contract_storage(&mut app);
+        let vault_contract = app.instantiate_contract(
+            contract_code_storage,
+            Addr::unchecked(DEPLOYER_ADDR),
+            &instantiate_msg,
+            &[],
+            "volatile_vault",
+            None
         ).unwrap();
 
 
         //TODO Check response attributes
 
         // Query and verify setup master
-        let setup_master: Option<Addr> = from_binary(
-            &query(deps.as_ref(), mock_env(), QueryMsg::SetupMaster {}).unwrap()
-        ).unwrap();
+        let setup_master: Option<Addr> = app
+            .wrap()
+            .query_wasm_smart::<SetupMasterResponse>(vault_contract.clone(), &QueryMsg::SetupMaster {})
+            .unwrap()
+            .setup_master;
 
         assert_eq!(
             setup_master.map(|account| account.to_string()),
@@ -36,9 +44,11 @@ mod test_volatile_instantiate {
         );
 
         // Query and verify chain interface
-        let chain_interface: Option<Addr> = from_binary(
-            &query(deps.as_ref(), mock_env(), QueryMsg::ChainInterface {}).unwrap()
-        ).unwrap();
+        let chain_interface: Option<Addr> = app
+            .wrap()
+            .query_wasm_smart::<ChainInterfaceResponse>(vault_contract.clone(), &QueryMsg::ChainInterface {})
+            .unwrap()
+            .chain_interface;
 
         assert_eq!(
             chain_interface.map(|account| account.to_string()),
@@ -46,9 +56,11 @@ mod test_volatile_instantiate {
         );
 
         // Query and verify OnlyLocal property
-        let only_local: bool = from_binary(
-            &query(deps.as_ref(), mock_env(), QueryMsg::OnlyLocal {}).unwrap()
-        ).unwrap();
+        let only_local: bool = app
+            .wrap()
+            .query_wasm_smart::<OnlyLocalResponse>(vault_contract.clone(), &QueryMsg::OnlyLocal {})
+            .unwrap()
+            .only_local;
 
         assert_eq!(
             only_local,
@@ -56,9 +68,11 @@ mod test_volatile_instantiate {
         );
 
         // Query and verify pool fee
-        let pool_fee: u64 = from_binary(
-            &query(deps.as_ref(), mock_env(), QueryMsg::PoolFee {}).unwrap()
-        ).unwrap();
+        let pool_fee: u64 = app
+            .wrap()
+            .query_wasm_smart::<PoolFeeResponse>(vault_contract.clone(), &QueryMsg::PoolFee {})
+            .unwrap()
+            .fee;
 
         assert_eq!(
             pool_fee,
@@ -66,9 +80,11 @@ mod test_volatile_instantiate {
         );
 
         // Query and verify governance fee
-        let gov_fee_share: u64 = from_binary(
-            &query(deps.as_ref(), mock_env(), QueryMsg::GovernanceFeeShare {}).unwrap()
-        ).unwrap();
+        let gov_fee_share: u64 = app
+            .wrap()
+            .query_wasm_smart::<GovernanceFeeShareResponse>(vault_contract.clone(), &QueryMsg::GovernanceFeeShare {})
+            .unwrap()
+            .fee;
 
         assert_eq!(
             gov_fee_share,
@@ -76,9 +92,10 @@ mod test_volatile_instantiate {
         );
 
         // Query and verify token info
-        let token_info: TokenInfo = from_binary(
-            &query(deps.as_ref(), mock_env(), QueryMsg::TokenInfo {}).unwrap()
-        ).unwrap();
+        let token_info: TokenInfo = app
+            .wrap()
+            .query_wasm_smart::<TokenInfo>(vault_contract.clone(), &QueryMsg::TokenInfo {})
+            .unwrap();
 
         assert_eq!(
             token_info,
@@ -97,35 +114,44 @@ mod test_volatile_instantiate {
     #[test]
     fn test_instantiate_only_local() {
 
-        let mut deps = mock_dependencies();
+        let mut app = App::default();
+
         let chain_interface = None;
         let instantiate_msg = mock_instantiate_msg(chain_interface);
 
 
         // Tested action: instantiate contract
-        let _response = instantiate(
-            deps.as_mut(),
-            mock_env(),
-            mock_info(DEPLOYER_ADDR, &vec![]),
-            instantiate_msg.clone()
+        let contract_code_storage = volatile_vault_contract_storage(&mut app);
+        let vault_contract = app.instantiate_contract(
+            contract_code_storage,
+            Addr::unchecked(DEPLOYER_ADDR),
+            &instantiate_msg,
+            &[],
+            "volatile_vault",
+            None
         ).unwrap();
 
 
         //TODO Check response attributes
 
         // Query and verify chain interface
-        let chain_interface: Option<Addr> = from_binary(
-            &query(deps.as_ref(), mock_env(), QueryMsg::ChainInterface {}).unwrap()
-        ).unwrap();
+        let chain_interface: Option<Addr> = app
+            .wrap()
+            .query_wasm_smart::<ChainInterfaceResponse>(vault_contract.clone(), &QueryMsg::ChainInterface {})
+            .unwrap()
+            .chain_interface;
 
-        assert!(
-            chain_interface.is_none()
+        assert_eq!(
+            chain_interface.map(|account| account.to_string()),
+            None
         );
 
         // Query and verify OnlyLocal property
-        let only_local: bool = from_binary(
-            &query(deps.as_ref(), mock_env(), QueryMsg::OnlyLocal {}).unwrap()
-        ).unwrap();
+        let only_local: bool = app
+            .wrap()
+            .query_wasm_smart::<OnlyLocalResponse>(vault_contract.clone(), &QueryMsg::OnlyLocal {})
+            .unwrap()
+            .only_local;
 
         assert_eq!(
             only_local,
