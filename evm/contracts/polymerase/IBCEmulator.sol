991c7986854093eb9bce07a1378790a4b1f1fbf6
Alexander <alexander@lindgren.xyz>
2023-05-02 17:05:03 +0200

    fix: Use another IBC-emulator

--- evm/contracts/polymerase/IBCEmulator.sol
+++ evm/contracts/polymerase/IBCEmulator.sol
@@ -2,51 +2,21 @@
 
 pragma solidity ^0.8.16;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "./IbcReceiver.sol";
 import "./IbcDispatcher.sol";
 
-struct PacketMetadata {
-    address target;
-    address sender;
-}
+contract IBCEmulator is IbcDispatcher, Ownable {
+    event Packet(IbcPacket packet);
 
-contract IBCEmulator is IbcDispatcher {
-    address[2] public _ports;
-
-    event IncomingMetadata(PacketMetadata metadata);
-    event IncomingPacket(IbcPacket packet);
-
-    event Acknowledgement(bytes acknowledgement);
-
-    function registerPort() external {
-        if (_ports[0] == address(0)) {
-            _ports[0] = msg.sender;
-        } else {
-            require(_ports[1] == address(0), "NO OPEN PORTS");
-            _ports[1] = msg.sender;
-        }
-    }
+    function registerPort() external {}
 
     function sendIbcPacket(
         bytes32 channelId,
         bytes calldata payload,
         uint64 timeoutBlockHeight
     ) external {
-        address target;
-        int128 port;
-        if (msg.sender == _ports[0]) {
-            target = _ports[1];
-            port = 1;
-        } else {
-            target = _ports[0];
-            port = 0;
-        }
-        // _packetTarget.push(PacketMetadata(target, msg.sender));
-
-        // bytes32(abi.encode((port - 1)**2)), bytes32(abi.encode(msg.sender)),
-        // bytes32(abi.encode(port)), bytes32(abi.encode(target)),
-        emit IncomingMetadata(PacketMetadata(target, msg.sender));
-        emit IncomingPacket(
+        emit Packet(
             IbcPacket(
                 IbcEndpoint(0, channelId),
                 IbcEndpoint(0, channelId),
@@ -57,17 +27,14 @@ contract IBCEmulator is IbcDispatcher {
         );
     }
 
-
     function execute(address targetContract, IbcPacket calldata packet)
-        external
-    {   
-        bytes memory acknowledgement = IbcReceiver(targetContract).onRecvPacket(packet);
-
-        emit Acknowledgement(acknowledgement);
+        external onlyOwner
+    {
+        IbcReceiver(targetContract).onRecvPacket(packet);
     }
 
     function timeout(address targetContract, IbcPacket calldata packet)
-        external
+        external onlyOwner
     {
         IbcReceiver(targetContract).onTimeoutPacket(packet);
     }
