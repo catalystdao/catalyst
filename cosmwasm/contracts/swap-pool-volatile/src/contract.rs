1f0128f899a47f22a05ac2cfa5ba2ef6be1d9556
Jorge Sanmiguel <sanmiguelgimeno@hotmail.com>
2023-05-10 19:31:49 +0200

    chore: Remove comment

--- cosmwasm/contracts/swap-pool-volatile/src/contract.rs
+++ cosmwasm/contracts/swap-pool-volatile/src/contract.rs
@@ -36,7 +36,6 @@ pub fn instantiate(
     msg: InstantiateMsg
 ) -> Result<Response, ContractError> {
 
-    //TODO move to 'setup'
     set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
 
     setup(
