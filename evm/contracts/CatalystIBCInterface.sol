27c95c8f4b718d1a9b2bfb78db035b01dcbf4213
Alexander <alexander@lindgren.xyz>
2023-05-22 15:20:06 +0200

    fix: It needs to be the dest channelid

--- evm/contracts/CatalystIBCInterface.sol
+++ evm/contracts/CatalystIBCInterface.sol
@@ -179,7 +179,7 @@ contract CatalystIBCInterface is Ownable, IbcReceiver {
 
         if (context == CTX0_ASSET_SWAP) {
             ICatalystV1Vault(fromVault).onSendAssetSuccess(
-                bytes32(packet.src.channelId),                                              // connectionId
+                bytes32(packet.dest.channelId),                                              // connectionId
                 data[ TO_ACCOUNT_LENGTH_POS : TO_ACCOUNT_END ],                             // toAccount
                 uint256(bytes32(data[ UNITS_START : UNITS_END ])),                          // units
                 uint256(bytes32(data[ CTX0_FROM_AMOUNT_START : CTX0_FROM_AMOUNT_END ])),    // fromAmount
@@ -189,7 +189,7 @@ contract CatalystIBCInterface is Ownable, IbcReceiver {
         }
         else if (context == CTX1_LIQUIDITY_SWAP) {
             ICatalystV1Vault(fromVault).onSendLiquiditySuccess(
-                bytes32(packet.src.channelId),                                              // connectionId
+                bytes32(packet.dest.channelId),                                              // connectionId
                 data[ TO_ACCOUNT_LENGTH_POS : TO_ACCOUNT_END ],                             // toAccount
                 uint256(bytes32(data[ UNITS_START : UNITS_END ])),                          // units
                 uint256(bytes32(data[ CTX1_FROM_AMOUNT_START : CTX1_FROM_AMOUNT_END ])),    // fromAmount
@@ -215,7 +215,7 @@ contract CatalystIBCInterface is Ownable, IbcReceiver {
 
         if (context == CTX0_ASSET_SWAP) {
             ICatalystV1Vault(fromVault).onSendAssetFailure(
-                bytes32(packet.src.channelId),                                              // connectionId
+                bytes32(packet.dest.channelId),                                              // connectionId
                 data[ TO_ACCOUNT_LENGTH_POS : TO_ACCOUNT_END ],                             // toAccount
                 uint256(bytes32(data[ UNITS_START : UNITS_END ])),                          // units
                 uint256(bytes32(data[ CTX0_FROM_AMOUNT_START : CTX0_FROM_AMOUNT_END ])),    // fromAmount
@@ -225,7 +225,7 @@ contract CatalystIBCInterface is Ownable, IbcReceiver {
         }
         else if (context == CTX1_LIQUIDITY_SWAP) {
             ICatalystV1Vault(fromVault).onSendLiquidityFailure(
-                bytes32(packet.src.channelId),                                              // connectionId
+                bytes32(packet.dest.channelId),                                              // connectionId
                 data[ TO_ACCOUNT_LENGTH_POS : TO_ACCOUNT_END ],                             // toAccount
                 uint256(bytes32(data[ UNITS_START : UNITS_END ])),                          // units
                 uint256(bytes32(data[ CTX1_FROM_AMOUNT_START : CTX1_FROM_AMOUNT_END ])),    // fromAmount
