f6e544639612cafb388d8a89e80f5fb356124d40
Jorge Sanmiguel <sanmiguelgimeno@hotmail.com>
2023-06-09 16:01:07 +0200

    chore: Rename swap-pool-volatile to catalyst-vault-volatile

--- cosmwasm/scripts/compile_contracts.sh
+++ cosmwasm/scripts/compile_contracts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd contracts
-for contract_name in "catalyst-factory" "swap-pool-volatile" "catalyst-ibc-interface" "catalyst-ibc-interface-poa"; do
+for contract_name in "catalyst-factory" "catalyst-vault-volatile" "catalyst-ibc-interface" "catalyst-ibc-interface-poa"; do
     cd $contract_name
     RUSTFLAGS='-C link-arg=-s' cargo build --release --lib --target=wasm32-unknown-unknown --locked
     cd ..
