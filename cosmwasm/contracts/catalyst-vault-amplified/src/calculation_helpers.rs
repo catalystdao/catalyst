4c8ae302d902bff32780651e809a5b1d27a00e1c
Jorge Sanmiguel <sanmiguelgimeno@hotmail.com>
2023-06-14 10:44:30 +0200

    feat: Implement calculation helpers + docs

--- cosmwasm/contracts/catalyst-vault-amplified/src/calculation_helpers.rs
+++ cosmwasm/contracts/catalyst-vault-amplified/src/calculation_helpers.rs
@@ -1,8 +1,7 @@
 use std::ops::Div;
-
 use catalyst_types::{U256, AsI256, AsU256, I256};
 use catalyst_vault_common::ContractError;
-use fixed_point_math::{ln_wad, div_wad_down, mul_wad_down, WAD, exp_wad, pow_wad, div_wad_up, WADWAD};
+use fixed_point_math::{div_wad_down, mul_wad_down, WAD, pow_wad, div_wad_up, WADWAD};
 
 
 // TODO use Uint128 where suitable instead of U256?
@@ -11,6 +10,9 @@ use fixed_point_math::{ln_wad, div_wad_down, mul_wad_down, WAD, exp_wad, pow_wad
 // TODO add overflow safety comments
 // Integral Helpers *************************************************************************************************************
 
+/// Compute the integral \int_{w a}^{w (a + input)} 1 / (x^k) · (1-k) dx
+///     = (w a + w input)^(1-k) - (w a)^(1-k) 
+/// NOTE: This function will revert for w == 0 or (a + input) == 0
 pub fn calc_price_curve_area(
     input: U256,
     a: U256,
@@ -18,24 +20,35 @@ pub fn calc_price_curve_area(
     one_minus_amp: I256
 ) -> Result<U256, ContractError> {
 
+    // Compute calc = (w a + w input)^(1-k)
     let mut calc = pow_wad(
-        w.checked_mul(a.checked_add(input)?)?.checked_mul(WAD)?.as_i256(),
+        w.checked_mul(a.checked_add(input)?)?
+            .checked_mul(WAD)?
+            .as_i256(), // If casting overflows to a negative number 'pow_wad' will fail
         one_minus_amp
     )?;
 
+    // Compute calc - (w a)^(1-k) 
+    // The calculation will fail for a vault balance of 0. Skip this case, as
+    // the result of the calculation will also be 0.
     if !a.is_zero() {
         calc = calc.wrapping_sub(
             pow_wad(
-                w.checked_mul(a)?.checked_mul(WAD)?.as_i256(),
+                w.checked_mul(a)?.checked_mul(WAD)?.as_i256(), // If casting overflows to a negative number 'pow_wad' will fail
                 one_minus_amp
             )?
         );
     }
 
-    Ok(calc.as_u256())
+    Ok(calc.as_u256())      // Casting always casts a positive number
 }
 
 
+/// Solve the equation u = \int_{w (a - y)}^{w a} 1 / (x^k) · (1-k) dx
+///     = b [ 1 - (
+///             ((w b) ^ (1-k) - u) / ((w b) ^ (1-k))
+///         )^( 1 / (1-k) )
+///     ]
 pub fn calc_price_curve_limit(
     u: U256,
     b: U256,
@@ -43,11 +56,13 @@ pub fn calc_price_curve_limit(
     one_minus_amp: I256
 ) -> Result<U256, ContractError> {
     
+    // Compute (w b)^(1-k)
     let weighted_balance_ampped = pow_wad(
-        w.checked_mul(b)?.checked_mul(WAD)?.as_i256(),
+        w.checked_mul(b)?.checked_mul(WAD)?.as_i256(),  // If casting overflows to a negative number 'pow_wad' will fail
         one_minus_amp
-    )?.as_u256();
+    )?.as_u256();                                       // Casting always casts a positive number
 
+    // Compute b [1 - ( (wbampped - u) / wbampped )^( 1/(1-k) )]
     mul_wad_down(
         b,
         WAD.wrapping_sub(
@@ -55,14 +70,17 @@ pub fn calc_price_curve_limit(
                 div_wad_up(
                     weighted_balance_ampped.checked_sub(u)?,
                     weighted_balance_ampped
-                )?.as_i256(),
+                )?.as_i256(),                           // Casting never overflows, as the division result is always <= 1
                 WADWAD.div(one_minus_amp)
-            )?.as_u256()
+            )?.as_u256()                                // Casting always casts a positive number
         )
     ).map_err(|err| err.into())
 }
 
 
+/// Solve the combined price equation. To reduce attack vectors, this is done with 
+/// the individual 'calc_price_curve_area' and 'calc_price_curve_limit' rather than with 
+/// the full simplified equation.
 pub fn calc_combined_price_curves(
     input: U256,
     a: U256,
@@ -80,9 +98,24 @@ pub fn calc_combined_price_curves(
 }
 
 
+/// Solve the 'liquidity to units' equation.    //TODO write equation as in the functions above
 pub fn calc_price_curve_limit_share(
     u: U256,
-    w_sum: U256
+    ts: U256,
+    n_weighted_balance_ampped: U256,
+    one_minus_amp_inverse: I256
 ) -> Result<U256, ContractError> {
-    todo!()
+    mul_wad_down(
+        ts,
+        (
+            pow_wad(
+                div_wad_down(
+                    n_weighted_balance_ampped.checked_add(u)?,
+                    n_weighted_balance_ampped
+                )?.as_i256(),                   // If casting overflows to a negative number 'pow_wad' will fail
+                one_minus_amp_inverse
+            )?.wrapping_sub(WAD.as_i256())      // Subtraction is underflow safe, as the result of 'pow_wad' is 
+                                                // always >= 1 (since the 'base' of the power is also >= 1)
+        ).as_u256()                             // Casting always casts a positive number
+    ).map_err(|err| err.into())
 }
