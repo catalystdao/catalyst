19436115b7cdd8f866606d02f95b956ac199846c
Jorge Sanmiguel <sanmiguelgimeno@hotmail.com>
2023-07-24 14:01:43 +0200

    test: Update amplified test parameters

--- cosmwasm/contracts/catalyst-vault-amplified/src/tests/parameters.rs
+++ cosmwasm/contracts/catalyst-vault-amplified/src/tests/parameters.rs
@@ -9,8 +9,8 @@ pub const DECIMALS_18: u128 = 1000000000000000000u128;
 pub const TEST_VAULT_ASSET_COUNT: usize = 3usize;
 
 pub const TEST_VAULT_BALANCES: &'static [Uint128] = &[
-    Uint128::new(100u128 * DECIMALS_18),
-    Uint128::new(20u128 * DECIMALS_18),
+    Uint128::new(100u128 * DECIMALS_6),
+    Uint128::new(20u128 * DECIMALS_6),
     Uint128::new(3u128 * DECIMALS_6)
 ];
 
@@ -18,5 +18,5 @@ pub const TEST_VAULT_BALANCES: &'static [Uint128] = &[
 pub const TEST_VAULT_WEIGHTS: &'static [Uint128] = &[
     Uint128::new(60000u128),
     Uint128::new(300000u128),
-    Uint128::new(2000000000000000000u128)
+    Uint128::new(2000000u128)
 ];
