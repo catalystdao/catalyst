2a54cbd79e0f5fc399d035f78604995b1aee6d74
Jorge Sanmiguel <sanmiguelgimeno@hotmail.com>
2023-07-26 10:57:48 +0200

    test: send asset event

--- cosmwasm/contracts/catalyst-vault-volatile/src/tests/test_send_asset.rs
+++ cosmwasm/contracts/catalyst-vault-volatile/src/tests/test_send_asset.rs
@@ -1,13 +1,13 @@
 mod test_volatile_send_asset {
-    use cosmwasm_std::{Uint128, Addr, Binary};
+    use cosmwasm_std::{Uint128, Addr, Binary, Attribute};
     use cw_multi_test::{App, Executor};
     use catalyst_types::U256;
     use catalyst_vault_common::{ContractError, msg::TotalEscrowedAssetResponse};
-    use test_helpers::{math::{uint128_to_f64, f64_to_uint128, u256_to_f64}, misc::{encode_payload_address, get_response_attribute}, token::{deploy_test_tokens, transfer_tokens, set_token_allowance, query_token_balance}, definitions::{SETUP_MASTER, CHANNEL_ID, SWAPPER_B, SWAPPER_A, FACTORY_OWNER}, contract::{mock_instantiate_interface, mock_factory_deploy_vault, DEFAULT_TEST_VAULT_FEE, DEFAULT_TEST_GOV_FEE, mock_set_vault_connection}};
+    use test_helpers::{math::{uint128_to_f64, f64_to_uint128, u256_to_f64, f64_to_u256}, misc::{encode_payload_address, get_response_attribute}, token::{deploy_test_tokens, transfer_tokens, set_token_allowance, query_token_balance}, definitions::{SETUP_MASTER, CHANNEL_ID, SWAPPER_B, SWAPPER_A, FACTORY_OWNER}, contract::{mock_instantiate_interface, mock_factory_deploy_vault, DEFAULT_TEST_VAULT_FEE, DEFAULT_TEST_GOV_FEE, mock_set_vault_connection}};
 
     use crate::{msg::VolatileExecuteMsg, tests::{helpers::{compute_expected_send_asset, volatile_vault_contract_storage}, parameters::{TEST_VAULT_BALANCES, TEST_VAULT_WEIGHTS, AMPLIFICATION, TEST_VAULT_ASSET_COUNT}}};
 
-    //TODO check event
+
 
     #[test]
     fn test_send_asset_calculation() {
@@ -145,6 +145,126 @@ mod test_volatile_send_asset {
             Addr::unchecked(invoked_interface),
             interface
         );
+
+    }
+
+
+    #[test]
+    fn test_send_asset_event() {
+
+        let mut app = App::default();
+
+        // Instantiate and initialize vault
+        let interface = mock_instantiate_interface(&mut app);
+        let vault_tokens = deploy_test_tokens(&mut app, SETUP_MASTER.to_string(), None, TEST_VAULT_ASSET_COUNT);
+        let vault_initial_balances = TEST_VAULT_BALANCES.to_vec();
+        let vault_weights = TEST_VAULT_WEIGHTS.to_vec();
+        let vault_code_id = volatile_vault_contract_storage(&mut app);
+        let vault = mock_factory_deploy_vault(
+            &mut app,
+            vault_tokens.iter().map(|token_addr| token_addr.to_string()).collect(),
+            vault_initial_balances.clone(),
+            vault_weights.clone(),
+            AMPLIFICATION,
+            vault_code_id,
+            Some(interface.clone()),
+            None
+        );
+
+        // Connect vault with a mock vault
+        let target_vault = encode_payload_address(b"target_vault");
+        mock_set_vault_connection(
+            &mut app,
+            vault.clone(),
+            CHANNEL_ID.to_string(),
+            target_vault.clone(),
+            true
+        );
+
+        // Define send asset configuration
+        let from_asset_idx = 0;
+        let from_asset = vault_tokens[from_asset_idx].clone();
+        let from_balance = vault_initial_balances[from_asset_idx];
+        let send_percentage = 0.15;
+        let swap_amount = f64_to_uint128(uint128_to_f64(from_balance) * send_percentage).unwrap();
+
+        let to_asset_idx = 1;
+        let min_out = f64_to_u256(12345.678).unwrap();  // Some random value
+
+        // Fund swapper with tokens and set vault allowance
+        transfer_tokens(
+            &mut app,
+            swap_amount,
+            from_asset.clone(),
+            Addr::unchecked(SETUP_MASTER),
+            SWAPPER_A.to_string()
+        );
+
+        set_token_allowance(
+            &mut app,
+            swap_amount,
+            from_asset.clone(),
+            Addr::unchecked(SWAPPER_A),
+            vault.to_string()
+        );
+
+
+
+        // Tested action: send asset
+        let response = app.execute_contract(
+            Addr::unchecked(SWAPPER_A),
+            vault.clone(),
+            &VolatileExecuteMsg::SendAsset {
+                channel_id: CHANNEL_ID.to_string(),
+                to_vault: target_vault.clone(),
+                to_account: encode_payload_address(SWAPPER_B.as_bytes()),
+                from_asset: from_asset.to_string(),
+                to_asset_index: to_asset_idx,
+                amount: swap_amount,
+                min_out,
+                fallback_account: SWAPPER_A.to_string(),
+                calldata: Binary(vec![])
+            },
+            &[]
+        ).unwrap();
+
+
+
+        // Check the event
+        let send_asset_event = response.events[1].clone();
+
+        assert_eq!(send_asset_event.ty, "wasm-send-asset");
+
+        assert_eq!(
+            send_asset_event.attributes[1],
+            Attribute::new("channel_id", CHANNEL_ID)
+        );
+        assert_eq!(
+            send_asset_event.attributes[2],
+            Attribute::new("to_vault", target_vault.to_string())
+        );
+        assert_eq!(
+            send_asset_event.attributes[3],
+            Attribute::new("to_account", encode_payload_address(SWAPPER_B.as_bytes()).to_base64())
+        );
+        assert_eq!(
+            send_asset_event.attributes[4],
+            Attribute::new("from_asset", from_asset)
+        );
+        assert_eq!(
+            send_asset_event.attributes[5],
+            Attribute::new("to_asset_index", to_asset_idx.to_string())
+        );
+        assert_eq!(
+            send_asset_event.attributes[6],
+            Attribute::new("from_amount", swap_amount)
+        );
+        assert_eq!(
+            send_asset_event.attributes[7],
+            Attribute::new("min_out", min_out)
+        );
+
+        // NOTE: the 'units' and 'fee' fields are indirectly checked on `test_send_asset_calculation`.
 
     }
 
