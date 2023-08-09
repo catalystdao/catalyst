f0d625289f134818c377f89370ae94a9cd2e8314
Alexander <alexander@lindgren.xyz>
2023-08-09 14:12:48 +0200

    feat: Tests & Coverage works using submodules

--- evm/test/CatalystInterface/setMinGasFor.t.sol
+++ evm/test/CatalystInterface/setMinGasFor.t.sol
@@ -12,24 +12,24 @@ contract TestSetMinGasFor is TestCommon {
     
     function test_min_gas_for_storage(bytes32 chainIdentifier, uint48 minGas) external {
         // Check that it is initially 0.
-        // assertEq(CCI.minGasFor(chainIdentifier), 0);
+        assertEq(CCI.minGasFor(chainIdentifier), 0);
         
-        // vm.prank(CCI.owner());
-        // CCI.setMinGasFor(chainIdentifier, minGas);
+        vm.prank(CCI.owner());
+        CCI.setMinGasFor(chainIdentifier, minGas);
 
         // Check that the storage updated.
-        // assertEq(CCI.minGasFor(chainIdentifier), minGas);
+        assertEq(CCI.minGasFor(chainIdentifier), minGas);
     }
 
     function test_min_gas_for_storage_only_owner(address caller) external {
-        // vm.assume(caller != CCI.owner());
+        vm.assume(caller != CCI.owner());
 
-        // vm.prank(caller);
-        // vm.expectRevert("Ownable: caller is not the owner");
-        // CCI.setMinGasFor(defaultExampleChainIdentifier, defaultExampleMinGas);
+        vm.prank(caller);
+        vm.expectRevert("Ownable: caller is not the owner");
+        CCI.setMinGasFor(defaultExampleChainIdentifier, defaultExampleMinGas);
 
         // Check that the storage isn't updated.
-        // assertNotEq(CCI.minGasFor(defaultExampleChainIdentifier), defaultExampleMinGas);
+        assertNotEq(CCI.minGasFor(defaultExampleChainIdentifier), defaultExampleMinGas);
     }
 }
 
