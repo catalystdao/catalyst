49a8ea69b9a5d2e094bab80b57ab8396b9c383e8
Alexander <alexander@lindgren.xyz>
2023-08-12 19:10:30 +0200

    feat: Update underwriting message size

--- evm/src/CatalystPayload.sol
+++ evm/src/CatalystPayload.sol
@@ -30,6 +30,31 @@ pragma solidity ^0.8.16;
 //       + FROM_AMOUNT      292 (32 bytes)
 //       + BLOCK_NUMBER     324 (4 bytes)
 //
+//    CTX2 - 0x02 - Please Underwrite Payload
+//      (Non-underwrite logic)
+//       + TO_ASSET_INDEX   228 (1 byte)
+//       + MIN_OUT          229 (32 bytes)
+//       + FROM_AMOUNT      261 (32 bytes)
+//       + FROM_ASSET_LEN   293 (1 byte)
+//       + FROM_ASSET       294 (64 bytes)
+//       + BLOCK_NUMBER     358 (4 bytes)
+//      (Fallback Logic)
+//       + TO_ACCOUNT_FALLBACK_LEN 262 (1 byte)
+//       + TO_ACCOUNT_FALLBACK     263 (64 bytes)
+//      (Underwrite Logic)
+//       + UW_INCENTIVE     327 (16 bytes)
+//
+//    CTX3 - 0x03 - Purpose Underwrite Payload
+//      (Fallback Logic)
+//       + TO_ASSET_INDEX   228 (1 byte)
+//      (Escrow Logic)
+//       + FROM_AMOUNT      229 (32 bytes)
+//       + FROM_ASSET_LEN   261 (1 byte)
+//       + FROM_ASSET       262 (64 bytes)
+//       + BLOCK_NUMBER     226 (4 bytes)
+//      (Underwrite Logic)
+//       + UNDERWRITE_ID    330 (32 bytes)
+// 
 // Common Payload (end)
 //    + DATA_LENGTH         LENGTH-N-2 (2 bytes)
 //    + DATA                LENGTH-N   (N bytes)
@@ -112,3 +137,61 @@ uint constant CTX1_DATA_LENGTH_START     = 328;
 uint constant CTX1_DATA_LENGTH_END       = 330;
 
 uint constant CTX1_DATA_START            = 330;
+
+
+
+// CTX2 Please Underwrite Payload ******************************************************************************************************
+
+uint constant CTX2_TO_ASSET_INDEX_POS    = 228;
+
+uint constant CTX2_MIN_OUT_START         = 229;
+uint constant CTX2_MIN_OUT_END           = 261;
+
+uint constant CTX2_FROM_AMOUNT_START     = 261;
+uint constant CTX2_FROM_AMOUNT_END       = 293;
+
+uint constant CTX2_FROM_ASSET_LENGTH_POS = 293; 
+uint constant CTX2_FROM_ASSET_START      = 294; 
+uint constant CTX2_FROM_ASSET_START_EVM  = 338;  // If the address is an EVM address, this is the start
+uint constant CTX2_FROM_ASSET_END        = 358;
+
+uint constant CTX2_BLOCK_NUMBER_START    = 358;
+uint constant CTX2_BLOCK_NUMBER_END      = 362;
+
+uint constant CTX2_TO_ACCOUNT_FALLBACK_LENGTH_POS   = 362;
+uint constant CTX2_TO_ACCOUNT_FALLBACK_START        = 363;
+uint constant CTX2_TO_ACCOUNT_FALLBACK_START_EVM    = 407;  // If the address is an EVM address, this is the start
+uint constant CTX2_TO_ACCOUNT_FALLBACK_END          = 427;
+
+uint constant CTX2_UW_INCENTIVE_START    = 427;
+uint constant CTX2_UW_INCENTIVE_END      = 443;
+
+uint constant CTX2_DATA_LENGTH_START     = 443;
+uint constant CTX2_DATA_LENGTH_END       = 445;
+
+uint constant CTX2_DATA_START            = 445;
+
+
+
+// CTX3 Purpose Underwrite Payload ******************************************************************************************************
+
+uint constant CTX3_TO_ASSET_INDEX_POS    = 228;
+
+uint constant CTX3_FROM_AMOUNT_START     = 229;
+uint constant CTX3_FROM_AMOUNT_END       = 261;
+
+uint constant CTX3_FROM_ASSET_LENGTH_POS = 261; 
+uint constant CTX3_FROM_ASSET_START      = 262; 
+uint constant CTX3_FROM_ASSET_START_EVM  = 306;  // If the address is an EVM address, this is the start
+uint constant CTX3_FROM_ASSET_END        = 326;
+
+uint constant CTX3_BLOCK_NUMBER_START    = 326;
+uint constant CTX3_BLOCK_NUMBER_END      = 330;
+
+uint constant CTX3_UNDERWRITE_ID_START   = 330;
+uint constant CTX3_UNDERWRITE_ID_END     = 362;
+
+uint constant CTX3_DATA_LENGTH_START     = 362;
+uint constant CTX3_DATA_LENGTH_END       = 364;
+
+uint constant CTX3_DATA_START            = 384;
