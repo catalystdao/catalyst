e548a74b4f5cb09893fc7c39e8661e800b657b56
Jorge Sanmiguel <sanmiguelgimeno@hotmail.com>
2023-08-28 11:25:58 +0200

    tests: Enable instantiate tests (no update)

--- cosmwasm/contracts/catalyst-vault-amplified/src/tests.rs
+++ cosmwasm/contracts/catalyst-vault-amplified/src/tests.rs
@@ -5,7 +5,7 @@ mod parameters;
 mod test_fees;
 mod test_finish_setup;
 // mod test_initialize_swap_curves;
-// mod test_instantiate;
+mod test_instantiate;
 mod test_local_swap;
 mod test_receive_asset;
 // mod test_receive_liquidity;
