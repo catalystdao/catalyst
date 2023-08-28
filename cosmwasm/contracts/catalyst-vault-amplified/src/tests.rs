54ec97f167c8fd1cac8d4b87cb288f613827df40
Jorge Sanmiguel <sanmiguelgimeno@hotmail.com>
2023-08-28 11:22:25 +0200

    test: Update finish setup tests

--- cosmwasm/contracts/catalyst-vault-amplified/src/tests.rs
+++ cosmwasm/contracts/catalyst-vault-amplified/src/tests.rs
@@ -3,7 +3,7 @@ mod parameters;
 // mod test_amplification_update;
 // mod test_deposit;
 mod test_fees;
-// mod test_finish_setup;
+mod test_finish_setup;
 // mod test_initialize_swap_curves;
 // mod test_instantiate;
 mod test_local_swap;
--- cosmwasm/contracts/catalyst-vault-volatile/src/tests.rs
+++ cosmwasm/contracts/catalyst-vault-volatile/src/tests.rs
@@ -2,7 +2,7 @@ mod helpers;
 mod parameters;
 // mod test_deposit;
 mod test_fees;
-// mod test_finish_setup;
+mod test_finish_setup;
 // mod test_initialize_swap_curves;
 // mod test_instantiate;
 mod test_local_swap;
