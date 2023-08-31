fb5dea50bb711e8662be4ccbcd7a313639ffc167
Alexander <alexander@lindgren.xyz>
2023-08-31 11:09:35 +0200

    chore: update swap script

--- evm/script/Swap.s.sol
+++ evm/script/Swap.s.sol
@@ -41,13 +41,13 @@ contract Swap is Script, IMessageEscrowStructs {
         uint256 deployerPrivateKey = vm.envUint("CATALYST_DEPLOYER");
         vm.startBroadcast(deployerPrivateKey);
 
-        address fromVault = address(0x7caF4D258906495515Af49aDc778cF9Ce711d5f4);
-        address toVault = address(0x7caF4D258906495515Af49aDc778cF9Ce711d5f4);
+        address fromVault = address(0x25A8aB870e20A239e94570eD6024f996283F7418);
+        address toVault = address(0x25A8aB870e20A239e94570eD6024f996283F7418);
 
 
         address WGAS = address(0xE67ABDA0D43f7AC8f37876bBF00D1DFadbB93aaa);
         Token(WGAS).approve(fromVault, 2**256-1);
-        IWETH(WGAS).deposit{value: uint256(0.001*1e18)}();
+        IWETH(WGAS).deposit{value: uint256(0.1*1e18)}();
 
         ICatalystV1Vault(fromVault).sendAsset{value: 2000000 * 10 gwei + 2000000 * 10 gwei}(
             bytes32(uint256(80001)),
@@ -55,7 +55,7 @@ contract Swap is Script, IMessageEscrowStructs {
             abi.encodePacked(uint8(20), bytes32(0), abi.encode(address(this))),
             WGAS,
             0,
-            uint256(0.001*1e18),
+            uint256(0.1*1e18),
             0,
             address(this),
             IncentiveDescription({
