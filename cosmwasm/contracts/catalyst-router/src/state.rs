4c8e4c698a548b7b84c0df49486bfba7e44e4a57
Jorge Sanmiguel <sanmiguelgimeno@hotmail.com>
2023-09-15 17:00:58 +0200

    test: Router lock tests

--- cosmwasm/contracts/catalyst-router/src/state.rs
+++ cosmwasm/contracts/catalyst-router/src/state.rs
@@ -83,3 +83,98 @@ pub fn only_router(
 
     Ok(())
 }
+
+
+
+#[cfg(test)]
+mod lock_tests {
+    use cosmwasm_std::{testing::{mock_dependencies, mock_info, mock_env}, Addr};
+
+    use crate::{state::{unlock_router, get_router_locker}, error::ContractError};
+
+    use super::{lock_router, only_router};
+
+    const LOCKER: &str = "locker";
+
+
+    #[test]
+    fn test_router_lock_and_unlock() {
+
+        let mut deps = mock_dependencies();
+        let info = mock_info(LOCKER, &[]);
+
+
+
+        // Tested action 1: lock router
+        lock_router(&mut deps.as_mut(), info).unwrap();
+
+        // Verify lock state
+        assert_eq!(
+            get_router_locker(&deps.as_ref()).unwrap(),
+            Addr::unchecked(LOCKER)
+        );
+
+
+
+        // Tested action 2: unlock router
+        unlock_router(&mut deps.as_mut());
+
+        // Verify lock state
+        assert!(
+            get_router_locker(&deps.as_ref()).is_err(),
+        );
+    }
+
+
+    #[test]
+    fn test_router_lock_twice() {
+
+        let mut deps = mock_dependencies();
+        let info = mock_info(LOCKER, &[]);
+
+        // Lock the router
+        lock_router(&mut deps.as_mut(), info.clone()).unwrap();
+
+
+
+        // Tested action: try to lock again (reentry)
+        let result = lock_router(&mut deps.as_mut(), info);
+
+        // Make sure lock fails
+        assert!(matches!(
+            result.err().unwrap(),
+            ContractError::Unauthorized {}
+        ));
+
+    }
+
+
+    #[test]
+    fn test_only_router() {
+
+        let env = mock_env();
+
+
+
+        // Tested action 1: Only router true
+        let sender = env.contract.address.as_str();
+        let info = mock_info(sender, &[]);
+        let result = only_router(&env, info);
+
+        assert!(result.is_ok());
+
+
+
+        // Tested action 2: Only router false
+        let sender = "not-the-contract";
+        let info = mock_info(sender, &[]);
+        let result = only_router(&env, info);
+
+        assert!(matches!(
+            result.err().unwrap(),
+            ContractError::Unauthorized {}
+        ));
+
+    }
+
+}
