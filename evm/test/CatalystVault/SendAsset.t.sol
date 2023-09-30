058a2706d4436409866d921204024f65115fd62a
Alexander <alexander@lindgren.xyz>
2023-09-30 13:18:49 +0200

    feat: test for fees

--- evm/test/CatalystVault/SendAsset.t.sol
+++ evm/test/CatalystVault/SendAsset.t.sol
@@ -113,5 +113,73 @@ abstract contract TestSendAsset is TestCommon, AVaultInterfaces {
             hex""
         );
     }
+
+    function test_sendAsset_fee(uint32 swapSizePercentage, uint56 fee) external virtual {
+        address toAccount = address(this);
+        bytes32 channelId = bytes32(uint256(123));
+        address[] memory vaults = getTestConfig();
+        require(vaults.length >= 2, "Not enough vaults defined");
+
+        uint8 fromVaultIndex = 0;
+        uint8 toVaultIndex = 1;
+        uint8 fromAssetIndex = 0;
+        uint8 toAssetIndex = 0;
+
+        address fromVault = vaults[fromVaultIndex];
+        address toVault = vaults[toVaultIndex];
+
+        setConnection(fromVault, toVault, channelId, channelId);
+
+        uint256 initial_invariant = invariant(vaults);
+
+        address fromToken = ICatalystV1Vault(fromVault)._tokenIndexing(fromAssetIndex);
+
+        uint256 amount = getLargestSwap(fromVault, toVault, fromToken, ICatalystV1Vault(toVault)._tokenIndexing(toAssetIndex)) * uint256(swapSizePercentage) / (2**32 - 1);
+
+        Token(fromToken).approve(fromVault, amount);
+
+        ICatalystV1Structs.RouteDescription memory routeDescription = ICatalystV1Structs.RouteDescription({
+            chainIdentifier: channelId,
+            toVault: convertEVMTo65(toVault),
+            toAccount: convertEVMTo65(toAccount),
+            incentive: _INCENTIVE
+        });
+
+
+        uint256 ss = vm.snapshot();
+
+        uint256 unitsLowerAmount = ICatalystV1Vault(fromVault).sendAsset{value: _getTotalIncentive(_INCENTIVE)}(
+            routeDescription,
+            fromToken,
+            toAssetIndex, 
+            uint256(amount) - uint256(amount) * uint256(fee) / 10**18, 
+            uint256(amount)/2, 
+            toAccount,
+            0,
+            hex""
+        );
+
+        vm.revertTo(ss);
+
+        ICatalystV1Vault(fromVault).setVaultFee(fee);
+
+        uint256 unitsWithFee = ICatalystV1Vault(fromVault).sendAsset{value: _getTotalIncentive(_INCENTIVE)}(
+            routeDescription,
+            fromToken,
+            toAssetIndex, 
+            uint256(amount), 
+            uint256(amount)/2, 
+            toAccount,
+            0,
+            hex""
+        );
+
+
+        assertEq(
+            unitsLowerAmount,
+            unitsWithFee,
+            "Fee not correctly taken"
+        );
+    }
 }
 
