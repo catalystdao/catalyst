3c98608ad98d06d5d475cfa69db9da7f7fb9eed0
Jorge Sanmiguel <8038323+jsanmigimeno@users.noreply.github.com>
2023-10-11 18:07:20 +0200

    chore: Remove IBC interface from scripts

--- cosmwasm/scripts/deploy_contracts.sh
+++ cosmwasm/scripts/deploy_contracts.sh
@@ -22,12 +22,6 @@ export FACTORY_CODE_ID=$(echo $deploy_output | jq -r '.logs[0].events[-1].attrib
 echo "Code id: $FACTORY_CODE_ID"
 echo
 
-echo Deploy volatile interface
-deploy_output=$(echo $PASSWORD_LOCAL | wasmd tx wasm store ./target/wasm32-unknown-unknown/release/catalyst_ibc_interface.wasm --from $WALLET_NAME_LOCAL $TX_FLAG -y -b block --output json)
-export INTERFACE_CODE_ID=$(echo $deploy_output | jq -r '.logs[0].events[-1].attributes[-1].value')
-echo "Code id: $INTERFACE_CODE_ID"
-echo
-
 echo Deploy volatile vault
 deploy_output=$(echo $PASSWORD_LOCAL | wasmd tx wasm store ./target/wasm32-unknown-unknown/release/catalyst_vault_volatile.wasm --from $WALLET_NAME_LOCAL $TX_FLAG -y -b block --output json)
 export VOLATILE_CODE_ID=$(echo $deploy_output | jq -r '.logs[0].events[-1].attributes[-1].value')
